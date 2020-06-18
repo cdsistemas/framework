edd3088cc1505e8fd3c2c16469dfc58e7dfd3487
Victor Vilella <victor.cbl@gmail.com>
2020-06-18 15:11:39 -0300

    Colocando o valor 0 se o ID for vazio

--- src/Illuminate/Database/Eloquent/Builder.php
+++ src/Illuminate/Database/Eloquent/Builder.php
@@ -265,10 +265,13 @@ class Builder
      */
     public function find($id, $columns = ['*'])
     {
+
         if (is_array($id) || $id instanceof Arrayable) {
             return $this->findMany($id, $columns);
         }
-
+        if(empty($id)){
+            $id = 0;
+        }
         return $this->whereKey($id)->first($columns);
     }
 
