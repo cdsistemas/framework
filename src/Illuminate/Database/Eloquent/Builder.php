72d8c4d7bd37268563c162884e622373e85c76c0
Victor Vilella <victor.cbl@gmail.com>
2021-09-21 15:47:43 -0300

    Otimização para codeigniter

--- src/Illuminate/Database/Eloquent/Builder.php
+++ src/Illuminate/Database/Eloquent/Builder.php
@@ -4,6 +4,7 @@ namespace Illuminate\Database\Eloquent;
 
 use Closure;
 use BadMethodCallException;
+use Illuminate\Database\Query\Grammars\Grammar;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
 use Illuminate\Pagination\Paginator;
@@ -441,16 +442,32 @@ class Builder
      */
     public function get($columns = ['*'])
     {
+        $params = $this->query->getBindings();
+        $sql = $this->query->toSql();
         $builder = $this->applyScopes();
-
-        // If we actually found models we will also eager load any relationships that
-        // have been specified as needing to be eager loaded, which will solve the
-        // n+1 query issue for the developers to avoid running a lot of queries.
-        if (count($models = $builder->getModels($columns)) > 0) {
-            $models = $builder->eagerLoadRelations($models);
+        if(function_exists("get_instance")){
+            $CI = &get_instance();
+            $conn = $CI->db->conn_id;
+            $args = array_merge([$conn, $sql], $params);
+            $res = call_user_func_array("ibase_query", $args);
+            $models = [];
+            while ($model = ibase_fetch_object($res)){
+                $models[] = $model;
+            }
+            $models = $this->hydrate($models);
+            return $models;
+        } else {
+            // If we actually found models we will also eager load any relationships that
+            // have been specified as needing to be eager loaded, which will solve the
+            // n+1 query issue for the developers to avoid running a lot of queries.
+            if (count($models = $builder->getModels($columns)) > 0) {
+                $models = $builder->eagerLoadRelations($models);
+            }
+            return $builder->getModel()->newCollection($models);
         }
 
-        return $builder->getModel()->newCollection($models);
+
+
     }
 
     /**
